bbeafa7562b7f364ab6175c1064c4d778d6f1b38
bsudheer473 <37907175+bsudheer473@users.noreply.github.com>
2022-02-07 14:12:33 -0500

    Update server.rb

--- recipes/cookbook/apache/test/recipes/server.rb
+++ recipes/cookbook/apache/test/recipes/server.rb
@@ -5,14 +5,14 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+package 'httpd' do
+  action :install
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+file '/var/www/html/index.html' do
+   content '<h1>Hello,sudheer<h1>'
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
 end
